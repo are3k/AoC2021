3c14f86ae3b0d1b8af4a1a452a858e71ac92df30
Are Egner-Kaupang <egk@ssb.no>
2021-12-21 20:07:07 +0100

    day 12: complete

--- 12/12.py
+++ 12/12.py
@@ -1,32 +1,14 @@
-import json
-from time import sleep
-
-
 def part_one(caves):
-  return None
   paths = []
   paths.append([caves['start']['name']])
   finishedPaths = []
-  deletePaths = []
   newPaths = []
-  nb = 0
   while len(paths) > 0:
-    print('number of finished paths: ', len(finishedPaths))
-    duplicates = [path for path in finishedPaths if finishedPaths.count(path) > 1]
-    print('duplicates in finishedPaths: ', len(duplicates))
-    if len(paths) == 68:
-      print('paths: ', paths)
-    nb += 1
-    # print('antall nye stier: ', len(newPaths))
     if len(newPaths) > 0:
       paths = newPaths.copy()
     newPaths = []
-    duplicates = [path for path in paths if paths.count(path) > 1]
-    print('duplicates in paths: ', len(duplicates))
-    print('number of paths to check: ', len(paths))
     for i in range(len(paths)):
       for cc in caves[paths[i][-1]]['connected_to']:
-        # print('cc', cc)
         path = paths[i].copy()
         if caves[cc]['type'] != 'start':
           if caves[cc]['type'] == 'end':
@@ -37,39 +19,24 @@ def part_one(caves):
           elif caves[cc]['type'] == 'big':
             path.append(cc)
           elif caves[cc]['type'] == 'small':
-            # # print('har kommet til liten hule')
             if cc not in path:
-              # # print('har ikke vært her før, så jeg legger til')
               path.append(cc)
-        if path not in paths and path != paths[i]:
+        if len(path) > len(paths[i]):
           newPaths.append(path)
-          # print('newPaths: ', newPaths)
+    paths = newPaths.copy()
   return len(finishedPaths)
 
 def part_two(caves):
   paths = []
   paths.append([caves['start']['name']])
   finishedPaths = []
-  deletePaths = []
   newPaths = []
-  nb = 0
   while len(paths) > 0:
-    print('number of finished paths: ', len(finishedPaths))
-    duplicates = [path for path in finishedPaths if finishedPaths.count(path) > 1]
-    print('duplicates in finishedPaths: ', len(duplicates))
-    if len(paths) == 68:
-      print('paths: ', paths)
-    nb += 1
-    # print('antall nye stier: ', len(newPaths))
     if len(newPaths) > 0:
       paths = newPaths.copy()
     newPaths = []
-    duplicates = [path for path in paths if paths.count(path) > 1]
-    print('duplicates in paths: ', len(duplicates))
-    print('number of paths to check: ', len(paths))
     for i in range(len(paths)):
       for cc in caves[paths[i][-1]]['connected_to']:
-        # print('cc', cc)
         path = paths[i].copy()
         if caves[cc]['type'] != 'start':
           if caves[cc]['type'] == 'end':
@@ -78,27 +45,23 @@ def part_two(caves):
               endpath.append(cc)
               finishedPaths.append(endpath)
           elif caves[cc]['type'] == 'big':
-            #print('har kommet til stor hule')
             path.append(cc)
           elif caves[cc]['type'] == 'small':
-            #print('har kommet til liten hule')
             if cc not in path:
-              # # print('har ikke vært her før, så jeg legger til')
               path.append(cc)
             else:
               sds = []
               for s in path:
-                #print('s i sti: ', s)
                 if s.islower() and s != 'start' and s != 'end':
                   sds.append(s)
-              #print('sjekker små: ', sds)
               duplicates = [p for p in sds if sds.count(p) > 1]
               if len(duplicates) == 0:
                 path.append(cc)
-        if path not in paths and path != paths[i]:
+        if len(path) > len(paths[i]):
           newPaths.append(path)
-          # print('newPaths: ', newPaths)
+    paths = newPaths.copy()
   return len(finishedPaths)
+
 
 lines = []
 caves = {}
@@ -122,12 +85,9 @@ for line in lines:
     caves[tc]['connected_to'].append(fc)
   if tc not in caves[fc]['connected_to']:
     caves[fc]['connected_to'].append(tc)
-# print(json.dumps(caves, indent=2))
-
-
 
 print(
     f"""Day 12:
     first solution: {part_one(caves.copy())}
     second solution: {part_two(caves.copy())}"""
- )
+)
